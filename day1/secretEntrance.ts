3f39ccd75a89a2f7c8349f1abe2db45a7c1a528f
nearz <andrewjneary@gmail.com>
2025-12-06 08:49:18 -0500

    day1-part1

---
+++ day1/secretEntrance.ts
@@ -0,0 +1,38 @@
+import { readFileSync } from "fs";
+
+function secretEntrence(
+  start: number,
+  positions: number,
+  input: string,
+): number {
+  const dialTurns = readInput(input);
+  let count = 0;
+  let curr = start;
+
+  for (const d of dialTurns) {
+    const dir = d.slice(0, 1);
+    const turns = Number(d.slice(1));
+    curr =
+      dir === "L"
+        ? trueModulo(curr - turns, positions)
+        : trueModulo(curr + turns, positions);
+    curr === 0 ? count++ : count + 0;
+  }
+  return count;
+}
+
+function trueModulo(n: number, d: number): number {
+  return ((n % d) + d) % d;
+}
+
+function readInput(input: string): string[] {
+  const inputTxt = readFileSync(input, "utf-8");
+  const lines = inputTxt.split("\n").filter((line) => line !== "");
+  return lines;
+}
+
+function main() {
+  console.log(secretEntrence(50, 100, "input.txt"));
+}
+
+main();
